f560dae4927433cffc69d7e985f232e42152f25c
Pankhuri Srivastava <pankh11pantomath@gmail.com>
2022-06-16 00:05:16 +0530

    Y

---
+++ index.js
@@ -0,0 +1,35 @@
+const express= require('express');
+const app=express();
+const dotenv=require('dotenv');
+const mongoose= require('mongoose');
+//Import Routes
+const authRoute=require('./routes/auth');
+
+const postRoute= require('./routes/posts');
+
+dotenv.config();
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT,
+{
+    useNewUrlParser: true
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+},
+()=>{
+    console.log("Connected to Database");
+});
+
+//Middleware
+app.use(express.json());
+
+
+
+
+
+// Route Middlewares
+app.use('/api/user',authRoute);
+app.use('/api/posts', postRoute);
+
+app.listen(3000,()=> console.log("Server is up and running"));
