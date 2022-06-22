cdc8ba5934b0bc59e515aae02ba9306d1e7a604d
aabhash gauda <aabhashgauda1@gmail.com>
2022-06-22 18:25:01 +0530

    port made variable

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const port=process.env.PORT;
 const app=express();
 const dotenv=require('dotenv');
 const mongoose= require('mongoose');
@@ -35,4 +36,4 @@ app.get('/',(req,res)=>{
 app.use('/api/user',authRoute);
 app.use('/api/posts', postRoute);
 
-app.listen(3000,()=> console.log("Server is up and running"));
+app.listen(port,()=> console.log("Server is up and running"));
