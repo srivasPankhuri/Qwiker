9e909378be914dfd0b698877aa3ca1cd4428e890
Pankhuri Srivastava <pankh11pantomath@gmail.com>
2022-09-02 12:03:37 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,31 @@
 const router = require('express').Router();
 const verify=require('../verifyToken');
+const mongoose =require('mongoose')
+const Post=require('../model/Posts');
+
+//add these 2 lines to make sure the parsing functionality is passed on to access body
+router.use(require('express').json());
+router.use(require('express').urlencoded({ extended: true }));
 
 
-router.get('/',verify,(req,res)=>{
-    res.send(req.user);
+router.post('/createpost',verify,(req,res)=>{
+    const{title,body}=req.body
+    if(!title||!body){
+        res.status(422).json({error:"Add fields"})
+    }
+    // console.log(req.user)
+    // res.send("ok")
+    const post=new Post({
+        title,
+        body,
+        postedBy:req.user
+    })
+    post.save().then(result=>{
+        res.json({post:result})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
 })
 
 module.exports=router;
